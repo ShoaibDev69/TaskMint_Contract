a8157e654fc1b1c1ae4396f53205d48b4135d30b
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-08-16 11:30:34 +0530

    initial commit

--- test/TaskMintTest.t.sol
+++ test/TaskMintTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {TaskMint} from "../src/TaskMint.sol";
 
 contract TaskMintTest is Test {
